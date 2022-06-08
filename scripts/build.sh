f285ef355916d7eb61e5f4f20af49b69e0c5d375
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-08 13:55:16 +0200

    fix: make build.sh cd to correct dir, with chmod+x
    update the building command in README like near-examples/NFT

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-cd ..
-cd "`dirname $0`"
+cd "`dirname $0`"/../ft
 cargo build --all --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/*.wasm ./res/
