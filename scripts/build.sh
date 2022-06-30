19418515fdb0d1fa89e1b8a0b0e9fe7e89b5c860
KK <kkurach@users.noreply.github.com>
2022-06-30 15:37:47 +0200

    Fix broken build.sh in Near FT
    This file was changed recently and now it fails on copying, thus not updating the .wasm.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ cd "`dirname $0`"
 cd ft
 cargo build --all --target wasm32-unknown-unknown --release
 cd ..
-cp target/wasm32-unknown-unknown/release/*.wasm ./res/
+cp ft/target/wasm32-unknown-unknown/release/*.wasm ./res/
